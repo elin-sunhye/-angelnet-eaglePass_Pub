6c5fac20f2c70252857199d3f33f7e9383027adc
elin-sunhye <tjsgp43@gmail.com>
2025-04-02 22:31:29 +0900

    메인, 타석예약 변경

--- js/common.js
+++ js/common.js
@@ -35,61 +35,95 @@ $(document).on('click', '.tab-box__tab-top__btn-tab button', function () {
 });
 
 // 타석예약 시간 타석 클릭
+// $(document).on(
+//     'click',
+//     '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type',
+//     function () {
+//         if ($(this).text().trim() !== '이용중') {
+//             if ($(this).hasClass('on')) {
+//                 // 클릭한 버튼에서 'on' 클래스 제거
+//                 $(this).removeClass('on');
+//             } else {
+//                 // 모든 버튼에서 'on' 클래스 제거
+//                 $(
+//                     '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type'
+//                 ).removeClass('on');
+//                 // 클릭한 버튼에 'on' 클래스 추가
+//                 $(this).addClass('on');
+//             }
+//         }
+//     }
+// );
+
+// 타석예약 불가
+function used() {
+    $('.tab-box__tab-cont > div .floor__row .floor__row__col').each(
+        function () {
+            if ($(this).text().trim().includes('불가')) {
+                // 버튼 텍스트가 불가면
+                $(this).addClass('used'); // 사용중 클래스 추가
+            }
+        }
+    );
+}
+used();
+
+// 타석예약 분 이용
+function shortTime() {
+    $('.tab-box__tab-cont > div .floor__row .floor__row__col').each(
+        function () {
+            if ($(this).text().trim().includes('이용')) {
+                // 버튼 텍스트가 이용
+                $(this).addClass('short'); // short 클래스 추가
+            }
+        }
+    );
+}
+shortTime();
+
+// 타석예약 선택 이전 시간 체크
+// function before() {
+//     $(
+//         '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type'
+//     ).each(function () {
+//         if ($(this).find('span').length > 0) {
+//             $(this).addClass('before');
+//         }
+//     });
+// }
+// before();
+
+// 내 예약 모달 열기
 $(document).on(
     'click',
     '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type',
     function () {
-        if ($(this).text().trim() !== '이용중') {
-            if ($(this).hasClass('on')) {
-                // 클릭한 버튼에서 'on' 클래스 제거
-                $(this).removeClass('on');
-            } else {
-                // 모든 버튼에서 'on' 클래스 제거
-                $(
-                    '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type'
-                ).removeClass('on');
-                // 클릭한 버튼에 'on' 클래스 추가
-                $(this).addClass('on');
-            }
-        }
+        $(
+            '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type'
+        ).removeClass('on'); // 'open' 클래스 삭제
+        $(this).addClass('on'); // 'open' 클래스 추가
+        $('.my-booking').fadeIn(300); // 3초동안 fadeIn
     }
 );
 
-// 타석예약 이용중
-function used() {
+// 내 예약 모달 닫기
+$(document).on('click', '.btn-modal-pop-up-close', function () {
     $(
         '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type'
-    ).each(function () {
-        if ($(this).text().trim() === '이용중') {
-            // 버튼 텍스트가 이용중이면
-            $(this).addClass('used'); // 사용중 클래스 추가
-        }
-    });
-}
-used();
-
-// 타석예약 선택 이전 시간 체크
-function before() {
-    $(
-        '.tab-box__tab-cont > div .floor__row .floor__row__col:last-of-type'
-    ).each(function () {
-        if ($(this).find('span').length > 0) {
-            $(this).addClass('before');
-        }
-    });
-}
-before();
-
-// 타석예약 모달 열기
-$(document).on('click', '.btn-modal-pop-up', function () {
-    $('.btn-modal-pop-up').addClass('open'); // 'open' 클래스 추가
-    $('.pop-up').fadeIn(300); // 3초동안 fadeIn
+    ).removeClass('on'); // 'open' 클래스 삭제
+    $('.my-booking').fadeOut(300); // 3초동안 fadeOut
 });
 
-// 타석예약 모달 닫기
+// 배치도 모달 열기
+$(document).on('click', '.btn-modal-pop-up', function () {
+    $('.btn-modal-pop-up').addClass('open'); // 'open' 클래스 추가
+    $('.position').fadeIn(300); // 3초동안 fadeIn
+});
+
+// 배치도 모달 닫기
 $(document).on('click', '.btn-modal-pop-up-close', function () {
     $('.btn-modal-pop-up').removeClass('open'); // 'open' 클래스 제거
-    $('.pop-up').fadeOut(300); // 3초동안 fadeOut
+    $('.position').fadeOut(300); // 3초동안 fadeOut
 });
 
 // 상품 구매
